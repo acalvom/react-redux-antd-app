35a386fe3f82ee37f45cd76b4957afcf7be0672b
acalvom <andreac.3108@gmail.com>
2022-03-14 14:45:47 +0100

    Import Ant Design & Create layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Layout } from 'antd';
+const { Header, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      REACT APP
+      <Layout>
+        <Header className="app-header">Header</Header>
+        <Layout>
+          <Sider className="app-sider">Sider</Sider>
+          <Content className="app-content">Content</Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }
