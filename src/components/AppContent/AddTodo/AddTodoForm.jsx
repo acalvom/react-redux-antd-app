4b57d6721f048063e476a64a3fa8bc19def05a32
acalvom <andreac.3108@gmail.com>
2022-03-15 18:42:41 +0100

    Create component for Add Todo Form Add Reset fields button Add styles #create-todo

---
+++ src/components/AppContent/AddTodo/AddTodoForm.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { SyncOutlined, CheckOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Select } from "antd";
+const { Option } = Select;
+
+const AddTodoForm = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  return (
+    <Form form={form} name="addTodoForm" labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }} onFinish={onFinish}>
+      <Form.Item label="Task" name="task" rules={[{ required: true, message: "Enter a task" }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Assigned To" name="assignedTo" rules={[{ required: true, message: "Assign the task to somebody" }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Priority" name="priority" rules={[{ required: true, message: "Set the priority" }]}>
+        <Select allowClear>
+          <Option value="high">HIGH</Option>
+          <Option value="medium">MEDIUM</Option>
+          <Option value="low">LOW</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button className="save-todo-button" type="primary" htmlType="submit" icon={<CheckOutlined />}>
+          Save
+        </Button>
+        <Button htmlType="button" onClick={onReset} icon={<SyncOutlined />}>
+          Reset
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default AddTodoForm;
--- src/components/AppContent/TodosHeader.jsx
+++ src/components/AppContent/TodosHeader.jsx
@@ -1,8 +1,8 @@
 import React, { Fragment, useState } from "react";
-import { PageHeader, Button, Modal, Form, Input, Select } from "antd";
-
+import { PageHeader, Button, Modal } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
-const { Option } = Select;
+import AddTodoForm from "./AddTodo/AddTodoForm";
+
 const TodosHeader = () => {
   const title = "LIST OF TODOS";
 
@@ -12,17 +12,8 @@ const TodosHeader = () => {
     setIsModalVisible(true);
   };
 
-
   const handleCancel = () => {
     setIsModalVisible(false);
-  };
-
-  const onFinish = (values) => {
-    console.log("Success:", values);
-  };
-
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
   };
 
   return (
@@ -31,71 +22,13 @@ const TodosHeader = () => {
         title={title}
         extra={
           <Button className="add-todo-button" icon={<PlusOutlined />} onClick={handleAddTodo}>
-            {"Add Todo"}
+            Add Todo
           </Button>
         }
       />
+      
       <Modal title="Basic Modal" visible={isModalVisible} onCancel={handleCancel} footer={null}>
-        <Form
-          name="basic"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Task"
-            name="task"
-            rules={[
-              {
-                required: true,
-                message: "Please, enter a task",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Assigned To"
-            name="assignedTo"
-            rules={[
-              {
-                required: true,
-                message: "Assign the task to somebody",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item label="Priority" name="priority">
-            <Select allowClear>
-              <Option value="high">high</Option>
-              <Option value="medium">medium</Option>
-              <Option value="low">low</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+        <AddTodoForm />
       </Modal>
     </Fragment>
   );
