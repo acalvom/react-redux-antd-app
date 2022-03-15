66ae69a540ee74b282d69fe66bda9c3870f216e6
acalvom <andreac.3108@gmail.com>
2022-03-15 12:03:00 +0100

    Set priority to mock data #list-todos

--- src/redux/actionsCreators/todosCreators.js
+++ src/redux/actionsCreators/todosCreators.js
@@ -1,8 +1,8 @@
 const todosListMock = [
-    { id: 1, task: 'Clean bathroom', assignedTo: 'Andrea', isDone: false },
-    { id: 2, task: 'Buy tomatoes', assignedTo: 'Andrea', isDone: true },
-    { id: 3, task: 'Get the hair cut', assignedTo: 'Paula', isDone: false },
-    { id: 4, task: 'Visit dentist', assignedTo: 'Carlos', isDone: false }
+    { id: 1, task: 'Clean bathroom', assignedTo: 'Andrea', priority: 'medium', isDone: false },
+    { id: 2, task: 'Buy tomatoes', assignedTo: 'Andrea', priority: 'high', isDone: true },
+    { id: 3, task: 'Get the hair cut', assignedTo: 'Paula', priority: 'low', isDone: false },
+    { id: 4, task: 'Visit dentist', assignedTo: 'Carlos', priority: 'high', isDone: false }
 ]
 
 export const listTodos = () => {
