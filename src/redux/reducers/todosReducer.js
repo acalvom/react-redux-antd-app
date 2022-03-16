a49c655965e5b71f427b5a4213d6eac49efffcab
acalvom <andreac.3108@gmail.com>
2022-03-16 17:55:06 +0100

    Add PATCH end-point, action and reducer update todo #edit-status-todo

--- src/redux/reducers/todosReducer.js
+++ src/redux/reducers/todosReducer.js
@@ -9,6 +9,15 @@ const todosReducer = (state = [], action) => {
         case '@todos/delete':
             return [...state.filter(item => item.id !== action.payload)];
 
+        case '@todos/update':
+            // let ret = state.map(item => item.id === action.payload.id ? { ...item, isDone: action.payload.isDone } : { ...item })
+            return state.map(item => {
+                if (item.id === action.payload.id)
+                    return { ...item, isDone: action.payload.isDone }
+                else
+                    return { ...item }
+            });
+
         default:
             return state;
     }
