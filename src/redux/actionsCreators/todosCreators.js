a43d6779cb948436de6b85de9eeb2a81c70ca99b
acalvom <andreac.3108@gmail.com>
2022-03-16 10:28:23 +0100

    Add POST Todo end-point, action & reducer #create-todo

--- src/redux/actionsCreators/todosCreators.js
+++ src/redux/actionsCreators/todosCreators.js
@@ -1,12 +1,23 @@
-import { getTodos } from '../../services/todosAPI';
+import { getTodos, postTodo } from '../../services/todosAPI';
 
 export const listTodos = () => {
-    
+
     return async (dispatch) => {
         const todosList = await getTodos();
         dispatch({
             type: '@todos/list',
             payload: todosList
+        });
+    }
+}
+
+export const addTodo = (newTodo) => {
+
+    return async (dispatch) => {
+        const createdTodo = await postTodo(newTodo);
+        dispatch({
+            type: '@todos/add',
+            payload: createdTodo
         });
     }
 }
--- src/services/todosAPI.js
+++ src/services/todosAPI.js
@@ -1,13 +1,22 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:3001"
+const BASE_URL = "http://localhost:3001/todos"
 
 export const getTodos = async () => {
     try {
-        const res = await axios.get(BASE_URL + "/todos");
+        const res = await axios.get(BASE_URL);
         return res.data;
     } catch (error) {
         return error;
     }
 }
 
+
+export const postTodo = async (newTodo) => {
+    try {
+        const res = await axios.post(BASE_URL, newTodo);
+        return res.data;
+    } catch (error) {
+        return error;
+    }
+}
